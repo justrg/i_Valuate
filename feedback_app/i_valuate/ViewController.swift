b16c413d12d633a4eec3f90d2f4538821c95bd2a
Justin Gasta <justrg@umich.edu>
2016-05-26 21:34:54 -0400

    Array used to keep track of text

--- feedback_app/i_valuate/ViewController.swift
+++ feedback_app/i_valuate/ViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var q1: UITextField!
-
+    
+    var arrayOfAnswers = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         q1.keyboardType = UIKeyboardType.NumberPad
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func didChange(sender: AnyObject) {
+        arrayOfAnswers[0] = q1.text!
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
 
