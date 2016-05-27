a7789d4ef8bb57ad73f64304323435f8786e17b3
Justin Gasta <justrg@umich.edu>
2016-05-26 22:07:51 -0400

    finished layout of app and compiled email body text

--- feedback_app/i_valuate/ViewController.swift
+++ feedback_app/i_valuate/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var q1: UITextField!
+    @IBOutlet weak var q2: UITextField!
+    @IBOutlet weak var q3: UITextField!
     
     var arrayOfAnswers = [String]()
     
@@ -18,8 +20,21 @@ class ViewController: UIViewController {
         q1.keyboardType = UIKeyboardType.NumberPad
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    // Store each of the answers in the array of answers
     @IBAction func didChange(sender: AnyObject) {
         arrayOfAnswers[0] = q1.text!
+    }
+    @IBAction func didChange2(sender: AnyObject) {
+        arrayOfAnswers[1] = q2.text!
+    }
+    @IBAction func didChange3(sender: AnyObject) {
+        arrayOfAnswers[2] = q3.text!
+    }
+    
+    
+    // Compile the body text and open the mail app
+    @IBAction func submitPressed(sender: AnyObject) {
     }
     
     override func didReceiveMemoryWarning() {
